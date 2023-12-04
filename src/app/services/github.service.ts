2f194b2a7201ed70a0f578f877f92ac8a4520df9
Sofien Laghouanem <Sofienlaghouanem@gmail.com>
2023-12-04 04:49:52 +0100

    ...

--- src/app/services/github.service.ts
+++ src/app/services/github.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
+  private accessToken = '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'; 
+
   constructor(private http: HttpClient) {}
 
   fetchGitHubUserData(username: string): Observable<any> {
-    const userUrl = `https://api.github.com/users/CodeWizard-404`;
-    const reposUrl = `https://api.github.com/users/CodeWizard-404/repos`;
+    const userUrl = `https://api.github.com/users/${username}`;
+    const reposUrl = `https://api.github.com/users/${username}/repos`;
 
-    const userPromise = this.http.get(userUrl);
-    const reposPromise = this.http.get(reposUrl);
+    const userPromise = this.http.get(userUrl, { headers: this.getHeaders() });
+    const reposPromise = this.http.get(reposUrl, { headers: this.getHeaders() });
 
     return forkJoin([userPromise, reposPromise]);
+  }
+
+  private getHeaders(): HttpHeaders {
+    return new HttpHeaders({
+      'Authorization': `Bearer ${this.accessToken}`
+    });
   }
 }
